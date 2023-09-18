faba69e281767024e1ebd0f713c75f36ae8e086f
Crowdbotics <team@crowdbotics.com>
2023-09-18 15:10:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shopugolden-43437",
+  url: "https://shopugolden-43437.botics.co",
+  api: "https://shopugolden-43437.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
